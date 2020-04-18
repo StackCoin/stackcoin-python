2b21644d9d1dad8ca2d8ea8ff0963df5d192ac56
Jack Arthur Harrhy <me@jackharrhy.com>
2020-04-18 03:20:06 -0230

    note

--- examples/basic.py
+++ examples/basic.py
@@ -1,9 +1,11 @@
+# some hacks to work around pulling in stackcoin a directory above
 import sys
 import os.path
 
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 )
+# you don't need this in your code :)
 
 from stackcoin import StackCoin
 
