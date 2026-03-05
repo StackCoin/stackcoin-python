196ae93698f474d18e6057471b1fd2876c810d3f
Jack Arthur Harrhy <me@jackharrhy.com>
2026-03-05 00:24:46 -0330

    feat: add REST client and WebSocket gateway

--- stackcoin/stackcoin/__init__.py
+++ stackcoin/stackcoin/__init__.py
@@ -1,5 +1,7 @@
 """StackCoin Python SDK."""
 
+from .client import Client
 from .errors import StackCoinError
+from .gateway import Event, Gateway
 
-__all__ = ["StackCoinError"]
+__all__ = ["Client", "Event", "Gateway", "StackCoinError"]
